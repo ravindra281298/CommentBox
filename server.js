8dfcbeab64f7ba6a0a66fb9c7988a22b3ca679c2
ravindra281298 <ravindra281298@gmail.com>
2019-07-06 12:53:57 +0530

    Comments are taken from user to backend hurry...git add -A

--- server.js
+++ server.js
@@ -3,10 +3,16 @@ require('./models/db');
 const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
+const bodyparser = require('body-parser');
 
 const commentController = require('./controllers/commentController');
 
 var app = express();
+app.use(bodyparser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyparser.json())
 
 app.set('views', path.join(__dirname, '/views/'));
 app.engine('hbs', exphbs({extname: 'hbs', defaultLayout: 'mainLayout', layoutDir: __dirname + '/views/layouts/' }));
