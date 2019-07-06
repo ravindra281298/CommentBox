783f5955f01318e518611f1be9e458de0e1f809b
ravindra281298 <ravindra281298@gmail.com>
2019-07-06 12:43:01 +0530

    Frontend part done

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -2,7 +2,9 @@ const express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-    res.json('sample text');
+    res.render("employee/addOrEdit", {
+        viewTitle: "Insert Comment"
+    });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 require('./models/db');
 
 const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
 
 const commentController = require('./controllers/commentController');
 
 var app = express();
+
+app.set('views', path.join(__dirname, '/views/'));
+app.engine('hbs', exphbs({extname: 'hbs', defaultLayout: 'mainLayout', layoutDir: __dirname + '/views/layouts/' }));
+app.set('view engine', 'hbs');
 
 app.listen(3000, () => {
     console.log('Express server started at port 3000');
